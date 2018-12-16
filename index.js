0993f6946b5a222b0fe9137b67de8fa0c2fe64a7
Jelena Klenkar <jelena.klenkar@gmail.com>
2018-12-16 19:08:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({},obj,{[key]:value});
   
 }
-var obj = { prop: 1 }
+
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-   obj[prop2]=2
-   return obj
+   obj[key]=value;
+   return object;
   
 }
