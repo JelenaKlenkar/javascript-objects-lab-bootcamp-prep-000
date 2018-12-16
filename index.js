113bb9e640b07d4c21081c59f62d444e7594b5d1
Jelena Klenkar <jelena.klenkar@gmail.com>
2018-12-16 18:37:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-function objects(){
-  var objects = {recipes};
-}
+var recipes = new Object ({});
+
+var obj = { prop: 1 }
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({},obj,{[key]:value})
